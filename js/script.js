bd648d7c52493a5d830b15b1164a43e1c2089d0d
MatanatBakirova <mbekirova11@gmail.com>
2023-12-27 14:26:27 +0400

    JS tetbiq etməyə başladım

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// DOM-Document object model
+// document.querySelector(".fa-xmark").addEventListener('click', function(){
+//     alert("x iconu klinlendi")
+
+ 
+// })
+
+// function istifadeciniSalamla (name ,surname){
+//     console.log(`salam , Yeni istifadeci ${name} ${surname}`)
+// }
+// istifadeciniSalamla('Metanet', 'Bekirova')
+
+document.querySelector('.fa-xmark').addEventListener('click', function(){
+    document.querySelector('.overlay-menu').style.width="0"
+
+})
+document.querySelector('.fa-bars').addEventListener('click', function(){
+    document.querySelector('.overlay-menu').style.width="100%"
+})
